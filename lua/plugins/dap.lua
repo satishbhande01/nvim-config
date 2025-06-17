8bd64ca0b65f9aed439ce6d362ce1ebdd6a300e8
satishbhande01 <satish.bhande@outlook.com>
2025-06-17 22:31:47 +0530

    Added debugger keybinds

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -25,6 +25,13 @@ return {
 
       -- Keymaps...
       vim.keymap.set("n", "<F5>", function() dap.continue() end, { desc = "Start/Continue Debugging" })
+			vim.keymap.set("n", "<F10>", function() dap.step_over() end, { desc = "Step Over" })
+      vim.keymap.set("n", "<F11>", function() dap.step_into() end, { desc = "Step Into" })
+      vim.keymap.set("n", "<F12>", function() dap.step_out() end, { desc = "Step Out" })
+      vim.keymap.set("n", "<Leader>b", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "Set Conditional Breakpoint" })
+      vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end, { desc = "Open DAP REPL" })
+      vim.keymap.set("n", "<Leader>du", function() dapui.toggle() end, { desc = "Toggle DAP UI" })
 
       -- ✅ Load Python DAP config
       require("plugins.dap.python")
