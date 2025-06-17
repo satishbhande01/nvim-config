d840d53dd59c666c2ae3bd4ca2c6e96d491ade3f
satishbhande01 <satish.bhande@outlook.com>
2025-06-17 22:28:16 +0530

    Added debugger-dapUI

---
+++ lua/plugins/dap/python.lua
@@ -0,0 +1,19 @@
+local dap = require("dap")
+
+dap.adapters.python = {
+  type = 'executable',
+  command = vim.fn.stdpath("data") .. '/mason/packages/debugpy/venv/bin/python',
+  args = { '-m', 'debugpy.adapter' },
+}
+
+dap.configurations.python = {
+  {
+    type = 'python',
+    request = 'launch',
+    name = "Launch file",
+    program = "${file}", -- current buffer
+    pythonPath = function()
+      return vim.fn.stdpath("data") .. '/mason/packages/debugpy/venv/bin/python'
+    end,
+  },
+}
