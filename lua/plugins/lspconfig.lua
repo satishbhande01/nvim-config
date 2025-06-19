d583062d46491eff4064a8ea57e10c14c62d6d32
satishbhande01 <satish.bhande@outlook.com>
2025-06-19 19:58:46 +0530

    Added R prog lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,14 +3,19 @@ return {
   config = function()
     local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+		-- Python
     lspconfig.lua_ls.setup({})
     lspconfig.pyright.setup({
 			capabilities = capabilities,
 		})
+		-- Bash
 		lspconfig.bashls.setup({
 			filetypes={"sh"},
 			cmd = {"bash-language-server","start"},
+			capabilities = capabilities,
+		})
+		-- R prog
+		lspconfig.r_language_server.setup({
 			capabilities = capabilities,
 		})
     -- Add more setups as needed
--- lua/plugins/mason-lspconf.lua
+++ lua/plugins/mason-lspconf.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { "williamboman/mason.nvim" },
   config = function()
     require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls", "pyright","bashls" }, -- add more LSPs as needed
+      ensure_installed = { "lua_ls", "pyright","bashls","r_language_server" }, -- add more LSPs as needed
       automatic_installation = true,
     })
   end,
