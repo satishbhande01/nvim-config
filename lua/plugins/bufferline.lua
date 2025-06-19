ecff181744ddf4c6fd799cd5723a729305286be6
satishbhande01 <satish.bhande@outlook.com>
2025-06-20 00:26:17 +0530

    Added Bufferline for multiple files.

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- for file icons
+    },
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "buffers", -- use "tabs" to show tabpages instead
+          separator_style = "thin", -- "slant" | "thick" | "thin"
+          diagnostics = "nvim_lsp",  -- show LSP diagnostics
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "left",
+              separator = true,
+            },
+          },
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+          always_show_bufferline = true,
+        },
+      })
+
+      --keymaps for navigation
+      local keymap = vim.keymap.set
+      keymap("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+      keymap("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Previous Buffer" })
+      keymap("n", "<leader>bd", "<Cmd>bdelete<CR>", { desc = "Delete Current Buffer" })
+      keymap("n", "<leader>bp", "<Cmd>BufferLinePick<CR>", { desc = "Pick Buffer" })
+    end,
+  },
+}
