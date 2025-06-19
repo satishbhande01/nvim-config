8be75927f74a1cdc6eac5ef7a31c0e53b4ef98ac
satishbhande01 <satish.bhande@outlook.com>
2025-06-20 00:20:18 +0530

    Updated debug breakpoint keybind.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -33,8 +33,8 @@ return {
 			vim.keymap.set("n", "<F10>", function() dap.step_over() end, { desc = "Step Over" })
       vim.keymap.set("n", "<F11>", function() dap.step_into() end, { desc = "Step Into" })
       vim.keymap.set("n", "<F12>", function() dap.step_out() end, { desc = "Step Out" })
-      vim.keymap.set("n", "<Leader>b", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
-      vim.keymap.set("n", "<Leader>B", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "Set Conditional Breakpoint" })
+      vim.keymap.set("n", "<Leader>db", function() dap.toggle_breakpoint() end, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>dB", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "Set Conditional Breakpoint" })
       vim.keymap.set("n", "<Leader>dr", function() dap.repl.open() end, { desc = "Open DAP REPL" })
       vim.keymap.set("n", "<Leader>du", function() dapui.toggle() end, { desc = "Toggle DAP UI" })
 
