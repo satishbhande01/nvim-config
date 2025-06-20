f42db75b3414d6b42e4f5c82b69ee65537e2fc44
satishbhande01 <satish.bhande@outlook.com>
2025-06-20 09:56:15 +0530

    Add MIT license

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -19,8 +19,8 @@ return {
               separator = true,
             },
           },
-          show_buffer_close_icons = false,
-          show_close_icon = false,
+          show_buffer_close_icons = true,
+          show_close_icon = true,
           always_show_bufferline = true,
         },
       })
